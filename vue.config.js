71654461d30c4dde78816748e588919db81cf154
ant-agl <aglanton31@gmail.com>
2024-04-01 05:14:33 +0300

    Add favicon

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,25 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  pwa: {
+    iconPaths: {
+      favicon32: "icon.png",
+      favicon16: "icon.png",
+      appleTouchIcon: "icon.png",
+      maskIcon: "icon.png",
+      msTileImage: "icon.png",
+    },
+    manifestOptions: {
+      icons: [
+        // {
+        //   src: "/app/img/icon-app.png",
+        //   sizes: "275x275",
+        //   type: "image/png",
+        // },
+      ],
+      display: "standalone",
+    },
+    themeColor: "#f3f2e0",
+    appleMobileWebAppCapable: "yes",
+  },
 });
